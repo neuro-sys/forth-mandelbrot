ffe66e83c88f6e8855437d73d4a772f118d712cc
Firat Salgur <firatsalgur@protonmail.com>
2021-03-30 19:53:51 +0300

    Add Mandelbrot

---
+++ mandelbrot.fs
@@ -0,0 +1,82 @@
+\ Mandelbrot set plotter in forth with fixed-point arithmetic.
+\
+\ Change fixed point precision in fp.fs to get higher quality.
+
+\ Currently requires SDL 1.x via libtool. To make it work without SDL,
+\ remove all SDL related words, and implement put-pixel word yourself
+\ for your own platform.
+\
+
+require sdl.fs
+require fp.fs
+
+also sdl.fs
+also fp.fs
+
+vocabulary mandelbrot.fs also mandelbrot.fs definitions
+
+20 constant max-iter
+
+: co! ( addr -- )             dup rot swap cell + ! ! ;
+: co@ ( addr -- )             dup @ swap cell + @ ;
+: co+ ( F: x y u v -- x y )   rot + rot rot + swap ;
+: co-abs^2 ( F: x0 y0 -- u )  dup fp* swap dup fp* + ;
+
+variable x
+variable y
+: complex^2 ( : x0 y0 -- x1 y1) \ x^2 - y^2 + 2xyi
+  y ! x !
+
+  x @ dup fp* \ x^2
+  x @ y @ 2 i>fp fp* fp* \ 2xyi
+  y @ dup fp* \ y^2
+  rot swap - swap
+;
+
+: x-scale ( u1 -- fp ) #width  i>fp fp/ 3 5 if>fp fp* 2 5 if>fp negate + ;
+: y-scale ( u1 -- fp ) #height i>fp fp/ 2 0 if>fp fp* 1 0 if>fp negate + ;
+
+2variable z
+2variable c
+variable in-set?
+variable counter
+variable x
+variable y
+: plot-mandel ( x y -- )
+  y ! x !
+  x @ i>fp x-scale
+  y @ i>fp y-scale c co!
+  0 0 z co!
+  0 counter !
+
+  max-iter 0 do
+    z co@ complex^2
+    c co@ co+
+    z co! \ z = z^2 + c
+    counter @ 1+ counter !
+
+    \ if |z| > 2
+    z co@ co-abs^2 4 i>fp > if
+
+      counter @ i>fp
+      max-iter i>fp fp/
+      255 i>fp fp* fp>i
+      2 mod 255 *
+      dup dup set-color
+      x @ y @ put-pixel
+
+      leave
+    then
+  loop
+;
+
+: render
+  init-sdl
+  #height 1- 0 do #width 1- 0 do i j plot-mandel loop flip-screen loop
+  wait-key
+  sdl-quit
+;
+
+render bye
+
+previous definitions
---
+++ sdl.fs
@@ -0,0 +1,93 @@
+[undefined] sdl.fs [if]
+
+c-library sdl
+s" SDL" add-lib
+
+\c #include <SDL/SDL.h>
+
+c-function sdl-init             SDL_Init            n -- n
+c-function sdl-set-video-mode	SDL_SetVideoMode	n n n n -- a
+c-function sdl-flip             SDL_Flip            a -- n
+c-function sdl-quit             SDL_Quit            -- void
+c-function sdl-delay            SDL_Delay           n -- void
+c-function sdl-poll-event       SDL_PollEvent       a -- void
+
+end-c-library
+
+vocabulary sdl.fs also sdl.fs definitions
+
+$00000000	constant SDL_SWSURFACE
+$80000000   constant SDL_FULLSCREEN
+$00000020	constant SDL_INIT_VIDEO
+$0000FFFF	constant SDL_INIT_EVERYTHING
+$00000002	constant SDL_KEYDOWN
+
+32          constant sdl-pixels-offset
+24          constant sdl-event-type-size
+
+1024 constant #width
+768 constant #height
+
+#width 4 * constant #stride
+
+variable color 3 cells allot
+variable surface
+variable pixels
+
+variable sdl-event sdl-event-type-size allot
+
+: wait-key 
+  begin
+    sdl-event sdl-poll-event
+    sdl-event c@ SDL_KEYDOWN =
+  until
+;
+
+: set-color ( b g r -- )
+  color c!
+  color 1 + c!
+  color 2 + c!
+;
+
+: get-pixel-addr ( x y -- addr )
+  pixels @ -rot #stride * swap 4 * + +
+;
+
+: set-pixel ( addr -- )
+  dup color     c@ swap c!
+  dup color 1 + c@ swap 1 + c!
+      color 2 + c@ swap 2 + c!
+;
+
+: put-pixel ( x y -- )
+  get-pixel-addr set-pixel
+;
+
+: pixel-off? ( x y -- t )
+  dup #height >= swap 0 < or swap
+  dup #width >= swap 0 < or
+  or
+;
+
+: clear-screen ( -- )
+  #stride #height * pixels @ + pixels @ do
+    0 i c!
+  loop
+;
+
+: init-sdl
+  SDL_INIT_EVERYTHING sdl-init
+  0<> if ." Error sdl-init" exit then
+
+  #width #height 32 SDL_SWSURFACE sdl-set-video-mode
+  dup 0< if ." Error sdl-set-video-mode" exit then surface !
+
+  \ save screen buffer address
+  surface @ sdl-pixels-offset + @ pixels !
+;
+
+: flip-screen surface @ sdl-flip throw ;
+
+previous definitions
+
+[then]
