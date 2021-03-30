84b5bcb5c488f0329f1b04d4e710e16621685287
Firat Salgur <firatsalgur@protonmail.com>
2021-03-30 20:04:38 +0300

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# exocortex
 # forth-mandelbrot
+
+Mandelbrot set plotter in FORTH with fixed-point arithmetic.
+
+Change fixed point precision in fp.fs to get higher quality.
+
+Currently requires SDL 1.x via libtool. To make it work without SDL
+and/or with a different Forth implementation, remove all SDL related
+words, and implement put-pixel word yourself for your own platform.
+
+
--- mandelbrot.fs
+++ mandelbrot.fs
@@ -1,11 +1,10 @@
-\ Mandelbrot set plotter in forth with fixed-point arithmetic.
+\ Mandelbrot set plotter in FORTH with fixed-point arithmetic.
 \
 \ Change fixed point precision in fp.fs to get higher quality.
-
-\ Currently requires SDL 1.x via libtool. To make it work without SDL,
-\ remove all SDL related words, and implement put-pixel word yourself
-\ for your own platform.
 \
+\ Currently requires SDL 1.x via libtool. To make it work without SDL
+\ and/or with a different Forth implementation, remove all SDL related
+\ words, and implement put-pixel word yourself for your own platform.
 
 require sdl.fs
 require fp.fs
