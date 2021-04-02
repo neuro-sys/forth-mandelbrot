739dae2dd58023e454d4ee88b3c08c6f09f17d5a
Firat Salgur <firatsalgur@protonmail.com>
2021-04-02 13:05:06 +0300

    Update fp.fs

--- fp.fs
+++ fp.fs
@@ -2,7 +2,8 @@
 
 vocabulary fp.fs also fp.fs definitions
 
-8                               constant point      \ fractional point
+\ adjust fractional point depending on your system's cell size
+32                              constant point      \ fractional point
 1 point lshift                  constant fp-bitmask
 fp-bitmask 1-                   constant fp-fmask   \ fractional bit mask
 fp-fmask invert                 constant fp-imask   \ integer bit mask
@@ -15,11 +16,34 @@ fp-bitmask negate fp-bitmask *  constant min-fp     \ minimum value
 : fp>i    ( fp -- n )           point rshift ;
 : fp*     ( n0 n1 -- n2 )       fp-bitmask */ ;
 : fp/     ( n0 n1 -- n2 )       fp-bitmask swap */ ;
-: if>fp   ( n0 n1 -- fp )       i>fp fp10 fp/ swap i>fp + ;
 : i3>fp3  ( a b c - a b c )     i>fp rot i>fp rot i>fp rot ;
 : fp3>i3  ( a b c - a b c )     fp>i rot fp>i rot fp>i rot ;
 : fpfloor ( n0 - n1 )           fp-imask and ;
 : fpceil  ( n0 - n1 )           fp.bit/2 + fpfloor ;
+
+\ return base 10 digits
+: c10s    ( a -- b)             0 begin 1+ swap 10 / swap over 0> 0=
+                                  until swap drop ;
+
+\ convert a decimal fractional number in the form integer fractional
+: if>fp   ( i f -- fp )
+  dup i>fp swap
+  c10s          ( i f 10s )
+  0 do          ( i f )
+    fp10 fp/
+  loop
+  swap i>fp +
+;
+
+\ supply the fractional digit count
+: if2>fp   ( i f c -- fp )
+  >r ( i f )
+  i>fp
+  r> 0 do        ( i f )
+    fp10 fp/
+  loop
+  swap i>fp +
+;
 
 previous definitions
 
